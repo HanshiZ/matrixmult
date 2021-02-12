b6440230166d9732cda86b097d2f8c431f2f229a
HanshiZ <61664954+HanshiZ@users.noreply.github.com>
2021-02-12 14:54:52 -0500

    Update optMultiplication.c
    added a dummy test

--- 2/src/optMultiplication.c
+++ 2/src/optMultiplication.c
@@ -4,7 +4,19 @@
 
 #include "optMultiplication.h"
 #define NVEC 16
+#define DUMMY 0
+#if DUMMY
 
+/* Test dereference with * vs [] */
+void optMultiplication(double* output, double* input_matrix, double* input_vector,int N,int M,int B)
+{
+        B = M;
+        for(int i = 0; i < N; ++i)
+            for(int j = 0; j < B; ++j)
+                for(int k = 0; k < NVEC ; ++k)
+                    *(output + i*NVEC + k) += *(input_matrix + i*M + j) * *(input_vector+j*NVEC+k);
+}
+#else
 /*
   Divide the columns into B blocks and then do the computations to avoid
   cache misses
@@ -12,10 +24,10 @@
 void optMultiplication(double* output, double* input_matrix, double* input_vector,int N,int M,int B)
 {
     /* I believe this is the wanted implementation */
-    int Bleng = M/B;
-    for(int h = 0; h < M; h+=Bleng)
+    for(int h = 0; h < M; h += B)
         for(int i = 0; i < N; ++i)
-            for(int j = h, end = h + Bleng; j < M && j < end; ++j)
+            for(int j = h, end = ((h + B) < M)? h + B : M; j < end; ++j)
                 for(int k = 0; k < NVEC ; ++k)
-                    *(output + i*NVEC + k) += *(input_matrix + i*M + j) * *(input_vector+j*NVEC+k);
+                   *(output + i*NVEC + k) += *(input_matrix + i*M + j)* *(input_vector+j*NVEC+k);
 }
+#endif
